8ac2ba8c30e83e4eab2b122331d19f8e609cb232
Igor Costa <igorcosta.silva10a@gmail.com>
2026-05-15 16:27:54 -0300

    feat: adiciona tabela produto

--- sql/01-schema.sql
+++ sql/01-schema.sql
@@ -7,11 +7,16 @@ create table cliente (
   nome varchar(100) not null,
   primary key (idcliente)
 );
-    
+
 create table vendedor (
   idvendedor int,
   nome varchar(100) not null,
   primary key (idvendedor)
 );
-	
 
+create table produto (
+	idproduto int,
+  nome varchar(100) not null,
+  preco decimal(10,2) not null,
+  primary key (idproduto)
+);
