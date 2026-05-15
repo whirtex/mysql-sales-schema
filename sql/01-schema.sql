64bde82f820b1acf30092e99030fed9953360311
Igor Costa <igorcosta.silva10a@gmail.com>
2026-05-15 16:40:05 -0300

    feat: adiciona tabela compra

--- sql/01-schema.sql
+++ sql/01-schema.sql
@@ -20,3 +20,15 @@ create table produto (
   preco decimal(10,2) not null,
   primary key (idproduto)
 );
+
+create table compra (
+	idcompra int,
+  idcliente int not null,
+  idvendedor int not null,
+  data date not null,
+  total decimal(10,2) not null,
+	primary key (idcompra),
+  foreign key (idcliente) references cliente(idcliente),
+  foreign key (idvendedor) references vendedor(idvendedor)
+);
+  
