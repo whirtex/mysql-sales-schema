34f301f23adbdf2e3f25420e38225b80f0dc2ee2
Igor Costa <igorcosta.silva10a@gmail.com>
2026-05-15 23:57:47 -0300

    feat: popula tabela vendedor

--- sql/02-inserts.sql
+++ sql/02-inserts.sql
@@ -11,4 +11,17 @@ insert into cliente (idcliente, nome) values
   (7,  'Bruno Almeida'),
   (8,  'Larissa Rocha'),
   (9,  'Gustavo Pereira'),
-  (10, 'Mariana Lopes');
+	(10, 'Mariana Lopes');
+
+-- populando vendedor
+insert into vendedor (idvendedor, nome) values
+  (1,  'Cláudio Coelho'),
+  (2,  'José Rubem'),
+  (3,  'Fábio Jr.'),
+  (4,  'Carlos Mendes'),
+  (5,  'Ana Beatriz'),
+  (6,  'Roberto Lima'),
+  (7,  'Juliana Alves'),
+  (8,  'Marcos Paulo'),
+  (9,  'Fernanda Dias'),
+  (10, 'Patrícia Gomes');
