576f97ad43875fc20fd86374f10a941d85522612
Igor Costa <igorcosta.silva10a@gmail.com>
2026-05-18 15:48:29 -0300

    feat: popula tabela produto

--- sql/02-inserts.sql
+++ sql/02-inserts.sql
@@ -25,3 +25,16 @@ insert into vendedor (idvendedor, nome) values
   (8,  'Marcos Paulo'),
   (9,  'Fernanda Dias'),
   (10, 'Patrícia Gomes');
+
+-- populando produto
+insert into produto (idproduto, nome, preco) values
+  (1,  'Banana',    2.50),
+  (2,  'Melancia',  3.90),
+  (3,  'Abacate',   6.50),
+  (4,  'Laranja',   3.00),
+  (5,  'Tangerina', 4.20),
+  (6,  'Maçã',      5.50),
+  (7,  'Uva',       9.90),
+  (8,  'Manga',     4.75),
+  (9,  'Abacaxi',   7.30),
+  (10, 'Morango',   11.50);
